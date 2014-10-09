2dc9272f8af86905871c64a37d04a54c8e0793c3
Nicolas Bush <nicolas@cordechasse.fr>
2014-10-09 17:20:37 +0200

    Initial check-in of module DailymotionBindingiOS

---
+++ DailymotionBinding/ApiDefinition.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace dailymotion {
+
+	[BaseType (typeof (UIViewController), Delegates= new string [] {"WeakDelegate"}, Events=new Type [] { typeof (DMPlayerDelegate) })]
+	interface DMPlayerViewController {
+
+		//------------------------------
+		//			CONSTRUCTOR
+		//------------------------------
+
+		[Export("initWithVideo:")]
+		IntPtr Constructor (string videoId);
+
+
+		[Export("initWithVideo:params:")]
+		IntPtr Constructor (string videoId, NSDictionary parameters);
+
+
+		//------------------------------
+		//			DELEGATE
+		//------------------------------
+
+		//delegate
+		[Export ("delegate", ArgumentSemantic.Assign)][NullAllowed]
+		NSObject WeakDelegate { get; set; }
+
+		[Wrap ("WeakDelegate")][NullAllowed]
+		DMPlayerDelegate Delegate { get; set; }
+
+
+
+		//------------------------------
+		//			METHODS
+		//------------------------------
+
+//		- (void)play;
+		[Export("play")]
+		void Play();
+
+//		- (void)togglePlay;
+		[Export("togglePlay")]
+		void TogglePlay();
+
+//		- (void)pause;
+		[Export("pause")]
+		void Pause();
+
+//		- (void)load:(NSString *)videoId;
+		[Export("load:")]
+		void Load(string videoId);
+
+//		- (void)api:(NSString *)method arg:(NSString *)arg;
+		[Export("api:arg:")]
+		void Api(string method, string arg);
+
+//		- (void)api:(NSString *)method;
+		[Export("api:")]
+		void Api(string method);
+
+
+
+
+
+
+
+		//------------------------------
+		//			PROPERTIES
+		//------------------------------
+
+		[Export ("autoplay")]
+		bool Autoplay { get; }
+
+		[Export ("currentTime")]
+		float CurrentTime { get; set; }
+
+		[Export ("bufferedTime")]
+		float BufferedTime { get; }
+
+		[Export ("duration")]
+		float Duration { get; }
+
+		[Export ("seeking")]
+		bool Seeking { get; }
+
+		[Export ("paused")]
+		bool Paused { get; }
+
+		[Export ("ended")]
+		bool Ended { get; }
+
+		[Export ("muted")]
+		bool Muted { get; set; }
+
+		[Export ("volume")]
+		float Volume { get; set; }
+
+		[Export ("fullscreen")]
+		bool Fullscreen { get; set; }
+
+		[Export ("error")]
+		NSError Error { get; }
+
+		[Export ("webBaseURLString")]
+		string WebBaseURLString { get; set; }
+
+	}
+
+
+	[BaseType (typeof (NSObject))]
+	[Model][Protocol]
+	interface DMPlayerDelegate {
+
+		[Export ("dailymotionPlayer:didReceiveEvent:"), EventArgs ("DMPlayerViewControllerDidReceiveEvent")]
+		void DidReceiveEvent (DMPlayerViewController player, string eventName);
+
+
+		/*
+		//methods delegate
+		//- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+		[Export("webView:shouldStartLoadWithRequest:navigationType:"), EventArgs ("DMPlayerViewControllerShouldStartLoad")]
+		bool ShouldStartLoad (UIWebView webView, MonoTouch.Foundation.NSUrlRequest request, UIWebViewNavigationType navigationType);
+
+		//		- (void)webViewDidStartLoad:(UIWebView *)webView
+		[Export("webViewDidStartLoad:"), EventArgs ("DMPlayerViewControllerLoadStarted")]
+		void LoadStarted (UIWebView webView);
+
+		//		- (void)webViewDidFinishLoad:(UIWebView *)webView
+		[Export("webViewDidFinishLoad:"), EventArgs ("DMPlayerViewControllerLoadingFinished")]
+		void LoadingFinished (UIWebView webView);
+
+		//		- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+		[Export("webView:didFailLoadWithError:"), EventArgs ("DMPlayerViewControllerLoadFailed")]
+		void LoadFailed (UIWebView webView, MonoTouch.Foundation.NSError error);
+		*/
+
+
+	}
+
+
+}
+
---
+++ DailymotionTester/DailymotionTesterViewController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using dailymotion;
+
+
+namespace DailymotionTester {
+
+	public partial class DailymotionTesterViewController : UIViewController {
+
+		public DailymotionTesterViewController (IntPtr handle) : base (handle) {
+		}
+
+		public override void DidReceiveMemoryWarning () {
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad () {
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			bt.TouchUpInside += (object sender, EventArgs e) => {
+
+				//@"webkit-playsinline": @(YES), @"related" : @(NO), @"logo" : @(NO), @"chromeless": @(YES), @"autoplay" : @(YES)}
+
+				NSDictionary videoParams = new NSMutableDictionary();
+				videoParams.SetValueForKey(new NSString("1"), new NSString("webkit-playsinline"));
+				videoParams.SetValueForKey(new NSString("0"), new NSString("related"));
+				videoParams.SetValueForKey(new NSString("0"), new NSString("logo"));
+				videoParams.SetValueForKey(new NSString("1"), new NSString("chromeless"));
+
+
+				DMPlayerViewController player = new DMPlayerViewController("x23bfnk", videoParams);
+				player.DidReceiveEvent += (object sender2, DMPlayerViewControllerDidReceiveEventEventArgs e2) => {
+					Console.WriteLine("ReceiveEvent");
+
+					if (e2.EventName == "apiready"){
+						player.Play();
+					}
+
+				};
+				AddChildViewController(player);
+				player.View.Frame = View.Bounds;
+				View.Add(player.View);
+
+			};
+
+		}
+
+		public override void ViewWillAppear (bool animated) {
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated) {
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated) {
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated) {
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
